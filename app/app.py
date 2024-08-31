f88b5d8f1ab28dc3639f690313b2f47d7e028187
Tommy <tokutomi@degino.com>
2024-08-31 21:42:32 +0900

    feat: プロジェクトを追加、表示する

--- app/app.py
+++ app/app.py
@@ -1,18 +1,58 @@
 # app.py
+from dataclasses import dataclass
+from datetime import datetime
+from uuid import UUID, uuid4
+
 import streamlit as st
 
-st.title("app")
 
-st.json({'foo':'bar','fu':'ba'})
+#
+# 型定義
+#
+@dataclass
+class Project:
+  id: UUID
+  title: str
+  created_at: datetime
 
-# Insert containers separated into tabs:
-tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+#
+# セッションステートの初期化
+#
+if "projects" not in st.session_state:
+  st.session_state.projects = [
+    Project(uuid4(), "プロジェクト1", datetime.now()),
+    Project(uuid4(), "プロジェクト2", datetime.now())
+    ]
 
-# You can also use "with" notation:
-with tab1:
-  st.radio('Select one:', [1, 2])
-  st.write("this is tab 1")
+def pane_projects():
+  """ プロジェクト一覧を表示する
+  """
+  header1, header2, header3 = st.columns(3)
+  header1.write("**ID**")
+  header2.write("**プロジェクト名**")
+  header3.write("**作成日時**")
 
-with tab2:
-  st.radio('Select one:', [3, 4])
-  st.write("this is tab 2")
+  for idx, project in enumerate(st.session_state.projects):
+    col1, col2, col3 = st.columns(3)
+    col1.write(idx+1)
+    col2.write(project.title)
+    col3.write(project.created_at.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+@st.dialog("新規プロジェクト作成")
+def create_project() -> None:
+  """ プロジェクトを作成するダイアログ
+  """
+  title = st.text_input("プロジェクト名")
+  if st.button("作成"):
+    project = Project(uuid4(), title, datetime.now())
+    st.session_state.projects.append(project)
+    st.rerun()
+  if st.button("キャンセル"):
+    st.rerun()
+
+
+if st.button("プロジェクトを作成する"):
+  create_project()
+st.divider()
+pane_projects()
--- app/pages/02_page.py
+++ app/pages/02_page.py
@@ -2,3 +2,31 @@
 import streamlit as st
 
 st.title("page2")
+
+# Insert containers separated into tabs:
+tab1, tab2, tab3, tab4 = st.tabs(["Tab 1", "Tab2", "Tab3", "Tab4"])
+
+# You can also use "with" notation:
+with tab4:
+  st.radio('Select one:', [1, 2])
+  st.write("this is tab 1")
+
+with tab2:
+  st.radio('Select one:', [3, 4])
+  st.write("this is tab 2")
+
+with tab3:
+  st.title("app")
+  st.json({'foo':'bar','fu':'ba'})
+
+with tab1:
+
+  if "value" not in st.session_state:
+      st.session_state.value = "Title2"
+
+  ##### Option using st.rerun #####
+  st.header(st.session_state.value)
+
+  if st.button("Foo"):
+      st.session_state.value = "Foo"
+      st.rerun()
